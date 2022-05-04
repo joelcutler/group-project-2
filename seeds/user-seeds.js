a53f47d128a5c6f43976266f24a8660481e36174
M8MBA <sauce.ix@gmail.com>
2022-05-03 20:27:25 -0600

    seeds user-seeds update

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,58 +3,80 @@ const { User, Post } = require('../models');
 
 const userData = [
   {
+    username: 'joel',
+    email: 'joel@joel.com',
+    password: 'pwpw',
+    avatar: '🏂'
+  },
+  {
+    username: 'jake',
+    email: 'jake@jake.com',
+    password: 'pwpw',
+    avatar: '🚴'
+  },
+  {
     username: 'jimi09',
     email: 'jimi09@cbc.ca',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '⛷️',
   },
   {
     username: 'zappa77',
     email: 'zappa77@sogou.com',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🏄‍♀️'
   },
   {
     username: 'paul44',
     email: 'paul44@last.fm',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🚣'
   },
   {
     username: 'danman1',
     email: 'danman1@gmail.com',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🏂'
   },
   {
     username: 'dirk44',
     email: 'dirk44@weather.com',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '⛷️'
   },
   {
     username: 'missmay66',
     email: 'missmay66@imdb.com',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🧗‍♀️'
   },
   {
     username: 'ski4life',
     email: 'ski4life@feedburner.com',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🚴'
   },
   {
     username: 'pennies7',
     email: 'pennies7@china.com.cn',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🏄‍♀️'
   },
   {
     username: 'robbin4',
     email: 'robbin4@google.ru',
-    password: 'password123'
+    password: 'pwpw',
+    avatar: '🚣'
   },
-  // {
-  //   username: 'jdilla',
-  //   email: 'jdilla@epa.gov',
-  //   password: 'password123'
-  // },
-
+  {
+    username: 'jdilla',
+    email: 'jdilla@epa.gov',
+    password: 'pwpw',
+    avatar: '🏂'
+  }
 ];
 
-const seedUsers = () => User.bulkCreate(userData, {individualHooks: true});
+
+const seedUsers = () => User.bulkCreate(userData, { individualHooks: true });
 
 module.exports = seedUsers;
